b6c1a2924b1b17939222800e23c9bb8ac1ea526c
Alexis Beauregard <mariposa_lex99@yahoo.com>
2023-06-19 19:03:48 -0400

    Completes react routes lab

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,21 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, Route, Switch } from "react-router-dom";
+import Home from "./Home";
+import Movies from "./Movies";
+import Directors from "./Directors";
+import Actors from "./Actors";
 
 function NavBar() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div className="navbar">
+      <NavLink to="/" exact>
+        Home
+      </NavLink>
+      <NavLink to="/movies">Movies</NavLink>
+      <NavLink to="/directors">Directors</NavLink>
+      <NavLink to="/actors">Actors</NavLink>
+    </div>
+  );
 }
 
 export default NavBar;
